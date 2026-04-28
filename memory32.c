5a420cf4fa812daad5f78bae51728d7c3bf2e53c
jdeepak1218 <jdeepak1218@gmail.com>
2026-04-28 21:57:27 +0530

    Fix: pass CPU pointer to paging translate in mem_read/mem_write to avoid NULL cpu on page faults

--- memory32.c
+++ memory32.c
@@ -1,19 +1,64 @@
 #include <stdint.h>
 #include "mmu.h"
 #include "cpu32.h"
-uint32_t mem_read32(CPU32 *cpu,uint32_t address)
+
+uint32_t mem_read32(CPU32 *cpu, uint32_t vaddr)
 {
-  return mmu_read32(cpu->mmu,address);
+    MMU *mmu = cpu->mmu;
+    mmu->reads++;
+    uint32_t paddr = 0;
+    if (mmu->paging_enabled) {
+        if (paging_translate(mmu, cpu, vaddr, 0, 0, &paddr) < 0)
+            return 0;
+    } else {
+        paddr = vaddr;
+    }
+    uint32_t val = 0;
+    phys_read32(mmu, paddr, &val);
+    return val;
 }
-void mem_write32(CPU32 *cpu,uint32_t address,uint32_t value)
+
+void mem_write32(CPU32 *cpu, uint32_t vaddr, uint32_t value)
 {
-  mmu_write32(cpu->mmu,address,value);
+    MMU *mmu = cpu->mmu;
+    mmu->writes++;
+    uint32_t paddr = 0;
+    if (mmu->paging_enabled) {
+        if (paging_translate(mmu, cpu, vaddr, 1, 0, &paddr) < 0)
+            return;
+    } else {
+        paddr = vaddr;
+    }
+    phys_write32(mmu, paddr, value);
 }
-uint8_t mem_read8(CPU32 *cpu,uint32_t address)
+
+uint8_t mem_read8(CPU32 *cpu, uint32_t vaddr)
 {
-  return mmu_read8(cpu->mmu,address);
+    MMU *mmu = cpu->mmu;
+    mmu->reads++;
+    uint32_t paddr = 0;
+    if (mmu->paging_enabled) {
+        if (paging_translate(mmu, cpu, vaddr, 0, 0, &paddr) < 0)
+            return 0;
+    } else {
+        paddr = vaddr;
+    }
+    uint8_t val = 0;
+    phys_read8(mmu, paddr, &val);
+    return val;
 }
-void mem_write8(CPU32 * cpu,uint32_t address,uint8_t value)
+
+void mem_write8(CPU32 * cpu, uint32_t vaddr, uint8_t value)
 {
-  mmu_write8(cpu->mmu,address,value);
+    MMU *mmu = cpu->mmu;
+    mmu->writes++;
+    uint32_t paddr = 0;
+    if (mmu->paging_enabled) {
+        if (paging_translate(mmu, cpu, vaddr, 1, 0, &paddr) < 0)
+            return;
+    } else {
+        paddr = vaddr;
+    }
+    phys_write8(mmu, paddr, value);
 }
+
